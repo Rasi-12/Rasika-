de4914d6ed70407f305e26cf6b7957dcb25a5069
Rasi-12 <k.s.r.200604@gmail.com>
2024-07-26 12:23:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// // Function to navigate to the next genre page
+// function navigate(currentGenreId, nextGenreId) {
+//     // Hide the current genre page
+//     document.getElementById(currentGenreId).style.display = 'none';
+    
+//     // Show the next genre page
+//     document.getElementById(nextGenreId).style.display = 'block';
+// }
+
